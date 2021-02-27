7e782b1acb892b6bb58825ab28ad9bc3e7bd1767
Rodion Sibov <rodionsibov@gmail.com>
2021-02-27 13:36:37 +0200

    Update

--- vue-task-tracker/src/App.vue
+++ vue-task-tracker/src/App.vue
@@ -2,7 +2,8 @@
   <div class="container">
 
     <Header title="Task Tracker" />
-    
+    <Tasks :tasks="tasks" />
+
   </div>
 
   
@@ -11,11 +12,41 @@
 
 <script>
  import Header from './components/Header'
+ import Tasks from './components/Tasks'
 
  export default {
    name: 'App',
    components: {
      Header,
+     Tasks
+   },
+   data() {
+     return {
+       tasks: []
+     }
+   },
+   created() {
+     this.tasks = [
+       {
+	 id: 1,
+	 text: 'Doctors Appointment',
+	 day: 'March 1st at 2:30pm',
+	 reminder: true,
+       },
+       {
+	 id: 2,
+	 text: 'Meeting at School',
+	 day: 'March 3rd at 1:30pm',
+	 reminder: true,
+       },
+       {
+	 id: 3,
+	 text: 'Food Shopping',
+	 day: 'March 3rd at 11:00am',
+	 reminder: false,
+       },
+
+     ]
    }
  }
 </script>
