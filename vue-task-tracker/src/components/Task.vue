93a3fd06fcb4100fcd46319ab5497a4370a1bea3
Rodion Sibov <rodionsibov@gmail.com>
2021-03-03 15:04:13 +0200

    Update

--- vue-task-tracker/src/components/Task.vue
+++ vue-task-tracker/src/components/Task.vue
@@ -1,8 +1,11 @@
 <template>
-  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
     <h3>
       {{ task.text }}
-      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
     </h3>
 
     <p>{{ task.day }}</p>
@@ -15,12 +18,7 @@ export default {
   props: {
     task: Object,
   },
-  methods: {
-    onDelete(id) {
-      console.log(id)
-    }
-  }
-};
+}
 </script>
 
 <style scoped>
