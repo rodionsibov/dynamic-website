eca773c23129abdd2a2b173a75df46e85894b51a
Rodion Sibov <rodionsibov@gmail.com>
2021-03-09 00:20:47 +0200

    Update

--- vue-task-tracker/src/App.vue
+++ vue-task-tracker/src/App.vue
@@ -13,11 +13,13 @@
       @delete-task="deleteTask"
       :tasks="tasks"
     />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
@@ -25,6 +27,7 @@ export default {
   name: "App",
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask,
   },
