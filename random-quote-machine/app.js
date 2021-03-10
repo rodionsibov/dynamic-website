81c2b17b91e35157a1887acf5cb65b1f81898f42
Rodion Sibov <rodionsibov@gmail.com>
2021-03-10 13:58:56 +0200

    Update

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -4,26 +4,26 @@ const app = Vue.createApp({
             author: "Kevin Kruse",
             quote: "Life isn’t about getting and having, it’s about giving and being.",
             message: "Random Quote Machine",
-            tweetURL: null
+            backgroundColor: "red"
         }
     },
     methods: {
         async getQuotes() {
             const res = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
             const { quotes } = await res.json()
-            console.log(quotes)
+            // console.log(quotes)
 
             let random = Math.floor(Math.random() * quotes.length + 1)
 
             this.author = quotes[random].author
             this.quote = quotes[random].quote
+
+            let randomColor = Math.floor(Math.random() * 360)
+            document.querySelector('body').style.backgroundColor = `hsl(${randomColor}, 50%, 50%)`
+            document.querySelector('.fa-quote-left').style.color = `hsl(${randomColor}, 50%, 50%)`
+
         },
-        tweet() {
-            const tweetURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
-
-        }
     },
-
 })
 
 app.mount('#app')
