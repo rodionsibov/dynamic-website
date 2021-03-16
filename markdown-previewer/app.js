4cf04bd8df5f5cca91f63b2e4c599d12903eac91
Rodion Sibov <rodionsibov@gmail.com>
2021-03-16 15:29:02 +0200

    Update

--- markdown-previewer/app.js
+++ markdown-previewer/app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     template: `
     <div>
-        <h2 class="text-center m-4">Convert your Markdown</h2>
+        <h2 class="text-center m-4 text-white">Convert your Markdown</h2>
         <div class="row">
             <div class="col-6">
-                <h6 class="text-center">Enter your markdown here</h6>
+                <h6 class="text-center text-white">Enter your markdown here</h6>
                 <textarea class="form-control" id="editor" :value="text" @keyup="handleChange"></textarea>
             </div>
-            <div class="col-6" id="preview">
-                <h6 class="text-center">See the result</h6>
-                <div class="preview rounded" v-html="markdown">
+            <div class="col-6">
+                <h6 class="text-center text-white">See the result</h6>
+                <div class="preview rounded p-2" v-html="markdown" id="preview">
                 </div>
             </div>
         </div>
@@ -17,18 +17,46 @@ const app = Vue.createApp({
     `,
     data() {
         return {
-         text: '',
+            text: `
+# Heading
+## Heading 2
+
+This is a paragraph
+
+**This is a bolded text**
+
+> Block Quotes
+
+- list item 1
+- list item 2
+- list item 3
+
+[visit my website](https://github.com)
+
+This is a inline \`<div></div>\`
+
+This is a block of code
+
+\`\`\`
+let x = 1;
+let y = 2;
+let z = x + y;
+\`\`\`
+
+![Vue](https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg)
+    
+         `
         }
     },
     methods: {
-       handleChange(e) {
-           this.text = e.target.value
-       }
+        handleChange(e) {
+            this.text = e.target.value
+        }
     },
     computed: {
         markdown() {
-            return marked(this.text)
-        }
+            return marked(this.text, { breaks: true })
+        },
     }
 })
 
