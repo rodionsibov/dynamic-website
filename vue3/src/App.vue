d601e15dc56ab035df6441630a16f192a80cc283
Rodion Sibov <rodionsibov@gmail.com>
2021-03-18 18:31:27 +0200

    First commit

---
+++ vue3/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <UserProfile />
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile";
+
+export default {
+  name: "App",
+  components: {
+    UserProfile,
+  },
+};
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #f3f5fa;
+}
+</style>
