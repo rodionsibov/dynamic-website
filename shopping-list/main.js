3cf55aef3121583d1dc6629949a9fc419e985279
Rodion Sibov <rodionsibov@gmail.com>
2021-03-18 10:58:45 +0200

    Update

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
     },
     methods: {
         saveItem() {
-            if(this.newItem !== '')
+            // if(this.newItem !== '')
             this.items.push(this.newItem)
             this.newItem = ''
         },
