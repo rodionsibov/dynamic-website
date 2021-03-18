eaf4ac8c890b1c9b98dde953d1c1b83b900d6b85
Rodion Sibov <rodionsibov@gmail.com>
2021-03-18 12:39:42 +0200

    Update

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -4,18 +4,45 @@ const app = Vue.createApp({
             state: 'default',
             header: 'shopping list app',
             newItem: '',
-            items: []
+            items: [
+                {
+                    label: '10 party hats',
+                    purchased: false,
+                },
+                {
+                    label: '2 board games',
+                    purchased: true,
+                },
+                {
+                    label: '20 cups',
+                    purchased: false,
+                },
+            ]
         }
     },
     methods: {
         saveItem() {
-            // if(this.newItem !== '')
-            this.items.push(this.newItem)
+            if (this.newItem !== '')
+                this.items.push({
+                    label: this.newItem,
+                    purchased: false
+                })
             this.newItem = ''
         },
         changeState(newState) {
             this.state = newState
             this.newItem = ''
+        },
+        togglePurchased(item) {
+            item.purchased = !item.purchased
+        }
+    },
+    computed: {
+        characterCount() {
+            return this.newItem.length
+        },
+        reversedItems() {
+            return this.items.slice(0).reverse()
         }
     }
 })
