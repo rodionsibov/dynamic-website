08a2590769cf384f6a397ec1e86c28c6d2428192
Rodion Sibov <rodionsibov@gmail.com>
2021-03-23 17:20:36 +0200

    init

---
+++ calculator/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="calculator">
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>4</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: green;
+}
+
+.calculator {
+  
+}
+</style>
