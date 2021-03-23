0460fe370827143a4a3a42f28d91d2051340ef40
Rodion Sibov <rodionsibov@gmail.com>
2021-03-23 13:42:27 +0200

    update

--- adopt-pets/src/App.vue
+++ adopt-pets/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/cats">Cats</router-link> |
+    <router-link to="/dogs">Dogs</router-link> |
+    <router-link to="/pet">Pet</router-link>
   </div>
   <router-view/>
 </template>
--- adopt-pets/src/router/index.js
+++ adopt-pets/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
+import Pet from '../views/Pet.vue'
 
 const routes = [
   {
@@ -8,13 +11,21 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/cats',
+    name: 'Cats',
+    component: Cats
+  },
+  {
+    path: '/dogs',
+    name: 'Dogs',
+    component: Dogs
+  },
+  {
+    path: '/pet',
+    name: 'Pet',
+    component: Pet
+  },
+  
 ]
 
 const router = createRouter({
