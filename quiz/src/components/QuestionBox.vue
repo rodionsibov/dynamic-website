27f525748de525d2b7c80fd228d38f027e240efa
Rodion Sibov <rodionsibov@gmail.com>
2021-03-23 11:24:19 +0200

    update

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="question-box-container p-3 rounded">
-    <p v-html="currentQuestion.question"></p>
-    <hr />
+    <p class="fs-3" v-html="currentQuestion.question"></p>
 
     <ul class="list-group mb-3">
-      <li class="list-group-item" v-for="(answer, index) in answers" :key="index">{{ answer }}</li>
+      <li
+        class="list-group-item"
+        v-for="(answer, index) in answers"
+        :key="index"
+        @click="selectAnswer(index)"
+      >
+        {{ answer }}
+      </li>
     </ul>
 
     <button class="btn btn-primary">Submit</button>
-    <button class="btn btn-success ms-1" @click="next">Next</button>
+    <button class="btn btn-success ms-2" @click="next">Next</button>
   </div>
 </template>
 
@@ -18,11 +24,21 @@ export default {
     currentQuestion: Object,
     next: Function,
   },
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
+    },
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
     },
   },
   mounted() {
@@ -33,7 +49,11 @@ export default {
 
 <style>
 .question-box-container {
-  height: 50vh;
   background: lightgray;
+}
+
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
 }
 </style>
