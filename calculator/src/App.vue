7c084d2ab95ff4a02ffadb3dcfa85668ff2af9a9
Rodion Sibov <rodionsibov@gmail.com>
2021-03-23 18:06:46 +0200

    update

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-<div class="calculator">
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>4</button>
-</div>
+  <div class="calculator">
+    <div id="display" class="display">141</div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  data() {
+    return {
+      num: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      op: ['=', '+', '-', '/', '*', 'ac'],
+    }
+  }
 }
 </script>
 
@@ -20,11 +23,62 @@ export default {
 }
 
 body {
+  min-height: 100vh;
+  font-family: "Courier New", Courier, monospace;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 0;
-  background-color: green;
+  background-color: rgb(209, 248, 209);
 }
 
 .calculator {
-  
+  width: 250px;
+  height: 380px;
+  padding: 5px;
+  background-color: #111;
+  display: flex;
+  flex-wrap: wrap;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+}
+
+.display {
+  color: white;
+  width: 100%;
+  font-size: 40px;
+  text-align: right;
+  padding: 10px;
+}
+
+button {
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+  border-radius: 50%;
+  font-size: 20px;
+  border: none;
+}
+
+button.big-v {
+  height: 110px;
+  border-radius: 50px;
+}
+button.big-h {
+  width: 110px;
+  border-radius: 50px;
+}
+
+button.orange {
+  background-color: orange;
+  color: white;
+}
+
+button.dark-gray {
+  background-color: darkslategray;
+  color: white;
+}
+
+button.light-gray {
+  background-color: darkgray;
 }
 </style>
