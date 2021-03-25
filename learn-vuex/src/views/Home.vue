b77b57a18a1b4d345038ff91b9b5d0e2061af03b
Rodion Sibov <rodionsibov@gmail.com>
2021-03-25 20:30:36 +0200

    first commit

---
+++ learn-vuex/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter">0</div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      counter: 0
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
